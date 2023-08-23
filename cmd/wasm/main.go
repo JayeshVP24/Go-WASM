4c93706caefbd15b98a29beaadf0de24d34149de
JayeshVP24 <jayeshpotlabattini@gmail.com>
2023-08-23 19:05:49 +0530

    WASM Dom manipulation

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
 	fmt.Println("Go web assembly")
+	js.Global().Set("formatJSON", jsonWrapper())
+	<-make(chan bool)
+
+}
+
+func prettyJson(input string) (string, error) {
+	var raw any
+	if err := json.Unmarshal([]byte(input), &raw); err != nil {
+		return "", err
+	}
+	pretty, err := json.MarshalIndent(raw, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(pretty), nil
+}
+
+func jsonWrapper() js.Func {
+	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) != 1 {
+			result := map[string]any{
+				"error": "Invalid no of arguments passed",
+			}
+			return result
+		}
+		jsDoc := js.Global().Get("document")
+		if !jsDoc.Truthy() {
+			result := map[string]any{
+				"error": "Unable to get document object",
+			}
+			return result
+		}
+		jsonOuputTextArea := jsDoc.Call("getElementById", "jsonoutput")
+		if !jsonOuputTextArea.Truthy() {
+			result := map[string]any{
+				"error": "Unable to get output text area",
+			}
+			return result
+		}
+		inputJSON := args[0].String()
+		fmt.Printf("input %s\n", inputJSON)
+		pretty, err := prettyJson(inputJSON)
+		if err != nil {
+			errStr := fmt.Sprintf("unable to parse JSON. Error %s occurred\n", err)
+			result := map[string]any{
+				"error": errStr,
+			}
+			return result
+		}
+		jsonOuputTextArea.Set("value", pretty)
+		return nil
+
+	})
+	return jsonFunc
 }
